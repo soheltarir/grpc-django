e2fd9b8c654f34a81868cd3c6ae50060f484f85f
Sohel Tarir <sohel.tarir@aasaanjobs.com>
2019-10-01 03:45:02 +0530

    Updated version tag

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import codecs
+import pathlib
 
 try:
     codecs.lookup('mbcs')
@@ -11,12 +12,18 @@ from setuptools import find_packages, setup
 
 version = "1.0.0"
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
     name="grpc-django",
     version=version,
     packages=find_packages(exclude=["tests*", "manage.py", "docs"]),
     include_package_data=True,
-    python_requires='3.5.2',
+    python_requires='>3.5.0',
     install_requires=[
         "Django >= 1.9",
         "grpcio",
@@ -27,5 +34,16 @@ setup(
     ],
     author="Sohel Tarir",
     author_email="sohel.tarir@gmail.com",
-    description="GRPC integration with Django projects"
+    description="gRPC Integration with Django Framework",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/soheltarir/grpc-django",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
